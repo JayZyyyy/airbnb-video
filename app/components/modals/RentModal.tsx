62ca0ba97abf21266d7a3accca352811f2d2a509
JayZyyyy <1538415595@qq.com>
2023-09-15 00:27:34 +0800

    day4

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -7,12 +7,15 @@ import Heading from "../Heading";
 
 import { categories } from '../navbar/Categories';
 import CategoryInput from "../inputs/CategoryInput";
-import { FieldValues, useForm } from "react-hook-form";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import dynamic from 'next/dynamic'
 import CountrySelect from "../inputs/CountrySelect";
 import Counter from "../inputs/Counter";
 import ImageUpload from "../inputs/ImageUpload";
 import Input from "../inputs/Input";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 enum STEPS {
   CATEGORY = 0,
@@ -25,6 +28,7 @@ enum STEPS {
 
 const RentModal = () => {
   const rentModal = useRentModal()
+  const router = useRouter()
   const [isLoading, setIsLoading] = useState(false)
 
   const [step, setStep] = useState(STEPS.CATEGORY)
@@ -77,6 +81,29 @@ const RentModal = () => {
 
   const onNext = () => {
     setStep((value) => value + 1)
+  }
+
+  const onSubmit: SubmitHandler<FieldValues> = data => {
+    if (step !== STEPS.PRICE) {
+      return onNext()
+    }
+
+    setIsLoading(true)
+
+    axios.post('/api/listings', data)
+      .then(() => {
+        toast.success('Listing created!')
+        router.refresh()
+        reset()
+        setStep(STEPS.CATEGORY)
+        rentModal.onClose()
+      })
+      .catch(() => {
+        toast.error('Something went wrong.')
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
   }
 
   const actionLabel = useMemo(() => {
@@ -248,7 +275,7 @@ const RentModal = () => {
     <Modal 
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={onNext}
+      onSubmit={handleSubmit(onSubmit)}
       actionLabel={actionLabel}
       title="Airbnb your home"
       secondaryActionLabel={secondaryActionLabel}
